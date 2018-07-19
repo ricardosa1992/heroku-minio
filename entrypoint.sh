c13f21bbe693719a6fd7d1934f5b4e746dcfb614
koori69 <koori69@users.noreply.github.com>
2018-07-19 09:37:49 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,1 +1,6 @@
-minio server --address :$PORT /mnt/minio/data
+cd /minio
+wget https://dl.minio.io/server/minio/release/linux-amd64/minio
+chmod +x minio
+export $MINIO_ACCESS_KEY
+export $MINIO_SECRET_KEY
+./minio server --address :$PORT /mnt/minio/data
