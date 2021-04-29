62ffc86007a5a86bb6855ea73e15c033baeb0670
ricardosa1992 <21196320+ricardosa1992@users.noreply.github.com>
2021-04-29 17:55:15 -0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,4 +3,4 @@ wget https://dl.minio.io/server/minio/release/linux-amd64/minio
 chmod +x minio
 export MINIO_ACCESS_KEY="$ACCESS_KEY"
 export MINIO_SECRET_KEY="$SECRET_KEY"
-./minio server /mnt/minio/data
+./minio server --address :$PORT /mnt/minio/data
